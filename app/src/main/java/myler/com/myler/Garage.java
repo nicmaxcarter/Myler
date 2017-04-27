e0aab22387f35d15ff392c620bfedcebe65b8b96
nicmaxcarter <nicmaxcarter@gmail.com>
2017-04-26 22:21:27 -0400

    messing around still

--- app/src/main/java/myler/com/myler/Garage.java
+++ app/src/main/java/myler/com/myler/Garage.java
@@ -6,11 +6,23 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
 
 public class Garage extends AppCompatActivity {
+
+    private DatabaseReference mRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +38,31 @@ public class Garage extends AppCompatActivity {
                 onNewVehicle(view);
             }
         });
+
+        // Databae reference
+        mRef = FirebaseDatabase.getInstance().getReference();
+
+        mRef.push().setValue("ThirdTest");
+
+        // Basic Array Adapter
+        ArrayList<String> items = new ArrayList<String>();
+        items.add("first input");
+        items.add("second input");
+        items.add("third input");
+        items.add("fourth input");
+        items.add("fifth input");
+        ArrayAdapter<String> itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items);
+
+        ListView lv = (ListView) findViewById(R.id.lvItems);
+        lv.setAdapter(itemsAdapter);
+    }
+
+    public void onDataChange(DataSnapshot dataSnapshot) {
+
+    }
+
+    public void onCancelled(DatabaseError databaseError) {
+//        Log.d(TAG, "database error");
     }
 
     @Override
